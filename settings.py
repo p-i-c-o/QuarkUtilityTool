71ddc89bc543e451bb8cc0c286cbacb2c9688c8b
p-i-c-o <95228665+p-i-c-o@users.noreply.github.com>
2022-05-24 14:58:35 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+# SETTINGS
+
+# !!! Replace the examples with your own !!! 
+# For MacOS, USB devices are stored in the "/Volumes" directory, hence the QuarkPath and the UsbPath variables below
+
+QuarkPath = "/Volumes/N3QUARK"
+UsbPath = "/Volumes"
+
+ScriptsPath = "/Scripts"
